4029ed99f8e6a381691eee072c2a778e3b068b2b
Azat Yamalov <yamalov.azat@live.com>
2022-12-08 20:02:34 +0300

    refactor

--- nf-module.js
+++ nf-module.js
@@ -1,5 +1,6 @@
 import { web } from "@nfjs/back";
 import { upload, download } from './lib/file-storage.js';
+import { api } from "@nfjs/core";
 
 const meta = {
     require: {
@@ -8,8 +9,21 @@ const meta = {
 };
 
 function init() {
-    web.on('POST', '/@nfjs/upload', { middleware: ['session', 'auth', 'files'], override: true }, upload, { bucket: 'nfjs-uploads' });
-    web.on('GET', '/@nfjs/download/:fileName', { middleware: ['session', 'auth'], override: true }, download, { bucket: 'nfjs-uploads' });
+    web.on('POST', '/@nfjs/upload', { middleware: ['session', 'auth', 'files'], override: true }, (async (context) => {
+        try {
+            const { id, filename } = await upload(context.fileInfo, context);
+            context.send(JSON.stringify({
+                id: id,
+                filename: filename
+            }));
+        }
+        catch (error) {
+            const err = api.nfError(error, error.message);
+            context.send(err.json());
+        }
+    }));
+
+    web.on('GET', '/@nfjs/download/:fileName', { middleware: ['session', 'auth'], override: true }, download);
 }
 
 export {
